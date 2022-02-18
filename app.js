01bffc84159f306d71cce5464c64d179aeab28c7
Robert Xu <Robert_xu2010@hotmail.com>
2022-02-18 19:58:33 +1100

    Add Gameplay

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ function playMatch(){
             // Computer Choice
             const computerChoise = moves[Math.floor(Math.random()*moves.length)];
             // Here we call compareHands
-            console.log(computerChoise)
-            const playerChoice = "scissors";
+            const playerChoice = this.className;
             compareHands(playerChoice, computerChoise);
+
+
+            // Update Images
+            playerHand.src = `./assets/${this.className}.png`;
+            computerHand.src = `./assets/${computerChoise}.png`;
+            
         })
     });
 
@@ -22,58 +27,59 @@ function playMatch(){
 function compareHands(playerChoice, computerChoice){
     // Update text
     const playerWins = document.querySelector('.player-wins p');
-    console.log(playerWins.textContent);
+    const computerWins = document.querySelector('.computer-wins p');
+    const ties = document.querySelector('.ties p');
+   
+    //console.log(playerWins.textContent);
+    //console.log(computerWins.textContent);
+    //console.log(ties.textContent);
+    
 
-    if(playerChoice == computerChoice){
-
+    // checking for a tie
+    if(playerChoice === computerChoice){
+        ties.textContent = Number(ties.textContent) + 1;
+        return;
     }
+
+    // check for rock
+    if(playerChoice === 'rock'){
+        if(computerChoice === 'scissors'){
+            playerWins.textContent = Number(playerWins.textContent) + 1;
+            return;
+        } else{
+            computerWins.textContent = Number(computerWins.textContent) + 1;
+            return;
+        }
+    }
+
+    // check for paper
+    if(playerChoice === 'paper'){
+        if(computerChoice === 'rock'){
+            playerWins.textContent = Number(playerWins.textContent) + 1;
+            return;
+        } else{
+            computerWins.textContent = Number(computerWins.textContent) + 1;
+            return;
+        }
+    }
+
+    // check for scissors
+    if(playerChoice === 'scissors'){
+        if(computerChoice === 'paper'){
+            playerWins.textContent = Number(playerWins.textContent) + 1;
+            return;
+        } else{
+            computerWins.textContent = Number(computerWins.textContent) + 1;
+            return;
+        }
+    }
+
 }
 
-
-
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
-    if(playerSelection == computerSelection){
-        return(playerSelection + " and " + computerSelection + ".It's a tie!");
-    }
-    if(playerSelection == "ROCK"){
-        if(computerSelection == "SCISSORS"){
-            return("Rock smashes scissors! You win!");
-        } else{
-            return("Paper covers rock! You lose!")
-        }
-    }
-    if(playerSelection == "PAPER"){
-        if(computerSelection == "ROCK"){
-            return("Paper covers rock! You win!");
-        } else{
-            return("Scissors cuts paper! You lose.");
-        }
-    }
-    if(playerSelection == "SCISSORS"){
-        if(computerSelection == "PAPER"){
-            return("Scissors cuts paper! You win!");
-        } else{
-            return ("Rock smashes scissors! You lose.");
-        }
-    }
-}
-
-
-function getPlayerSelection(message){
-    var playerSelection;
-    while(true){
-        playerSelection = prompt(message).toUpperCase();
-        if(playerSelection == "ROCK" || playerSelection == "PAPER" || playerSelection == "SCISSORS"){
-            break;
-        }
-    }
-    return playerSelection;
-}
 
 function game(){
     playMatch();
 }
 
+// start the game function
 game();
