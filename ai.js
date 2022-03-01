fdf3685fea8823cd13e80858aa2dec9e43284db3
Robert Xu <Robert_xu2010@hotmail.com>
2022-03-01 12:25:00 +1100

    completed nextMove

--- ai.js
+++ ai.js
@@ -1,4 +1,5 @@
 const moves = ["rock", "paper", "scissors"];
+const moveSize = 3;
 
 winWeight = 1;
 lossWeight = 0;
@@ -7,7 +8,38 @@ lossWeight = 0;
 function randomMove(){
     return moves[Math.floor(Math.random() * moves.length)];
 }
+  
+// returns the nth move after the given move, nextMove("rock", 2) = "scissors"
+function nextMove(move, n) {
+    var i = (moves.indexOf(move) + n) % moveSize;
+    return moves[i];
+}
 
+// AI LOGIC 1
+// assuming that the player picks the same move after winning, the next move after losing
+// random move if tie
 function sameNext(){
-    console.log(history[history.length - 1]);
+    var movePredict;
+
+    const play = history[history.length - 1];
+
+    console.log(play);
+
+    if(play.winner === "player"){
+        movePredict = nextMove(play.playerMove, 1);
+    } else if(play.winner === "computer"){
+        movePredict = nextMove(play.playerMove, 2);
+    } else{
+        movePredict = randomMove();
+    }
+
+    console.log(movePredict)
+
+    return movePredict;
+}
+
+// AI LOGIC 2
+// assumes that the player always picks the next move R -> P -> S -> R -> ...
+function sameSame(){
+    
 }
